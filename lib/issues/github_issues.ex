a4fdf1a5fd1db06932d6d334658e8d68374f15e7
icednut <wglee21g@gmail.com>
2023-01-24 22:37:48 +0900

    github 예제코드 작성

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,29 @@
+defmodule Issues.GithubIssues do
+
+  @github_url Application.get_env(:issues, :github_url)
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({:ok, %{status_code: status_code, body: body}}) do
+    {
+      status_code |> check_for_error(),
+      body |> Poison.Parse.parse!(%{})
+    }
+  end
+
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_), do: :error
+end
