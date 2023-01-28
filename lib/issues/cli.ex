7ae7971b269703b0e255a7d5f69ef98400d43955
icednut <wglee21g@gmail.com>
2023-01-28 13:04:15 +0900

    13장 실습

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,4 +1,5 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
 
   @default_count 4
   @moduledoc """
@@ -11,20 +12,36 @@ defmodule Issues.CLI do
   end
 
   def process(:help) do
-    IO.puts """
+    IO.puts("""
     usage: issues <user> <project> [count | #{@default_count}]
-    """
+    """)
+
     System.halt(0)
   end
 
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
+    |> last(_count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
   end
 
   def decode_response({:ok, body}), do: body
+
   def decode_response({:error, error}) do
-    IO.puts "Error fetching from Github: #{error["message"]}"
+    IO.puts("Error fetching from Github: #{error["message"]}")
     System.halt(2)
   end
 
